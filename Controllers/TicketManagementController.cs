1a6a48c5986cad79d9553de5ff764e9008180872
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-12-01 21:55:36 +0700

    add filter for ticketmanagement, refresh button

--- Controllers/TicketManagementController.cs
+++ Controllers/TicketManagementController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BookingTourWebApp_MVC.Data;
 using BookingTourWebApp_MVC.Models;
+using BookingTourWebApp_MVC.ViewModels;
 
 namespace BookingTourWebApp_MVC.Controllers
 {
@@ -20,10 +21,44 @@ namespace BookingTourWebApp_MVC.Controllers
         }
 
         // GET: Bookings
-        public async Task<IActionResult> Index()
+        public async Task<ActionResult<IEnumerable<Booking>>> Index(string? flightId, string? ticketId, string? departure, string? destination, DateTime departureTime, DateTime bookingTime)
         {
-            var applicationDbContext = _context.Bookings.Include(a => a.AppUser).Include(b => b.Flight);
-            return View(await applicationDbContext.ToListAsync());
+            //var applicationDbContext = _context.Bookings.Include(a => a.AppUser).Include(b => b.Flight);
+            //return View(await applicationDbContext.ToListAsync());
+            var query = _context.Bookings.Include(a => a.AppUser).Include(b => b.Flight).AsQueryable();
+
+            if (!string.IsNullOrEmpty(departure))
+            {
+                query = query.Where(f => f.Flight.Departure == departure);
+            }
+
+            if (!string.IsNullOrEmpty(destination))
+            {
+                query = query.Where(f => f.Flight.Destination == destination);
+            }
+
+            if (departureTime.ToString().Substring(0, 8) != "1/1/0001" && departureTime.ToString().Substring(0, 10) != "01/01/0001")
+            {
+                query = query.Where(f => f.Flight.DepartureTime.Year == departureTime.Year && f.Flight.DepartureTime.Month == departureTime.Month && f.Flight.DepartureTime.Day == departureTime.Day);
+            }
+
+            if (bookingTime.ToString().Substring(0, 8) != "1/1/0001" && bookingTime.ToString().Substring(0, 10) != "01/01/0001")
+            {
+                query = query.Where(f => f.BookingTime.Year == bookingTime.Year && f.BookingTime.Month == bookingTime.Month && f.BookingTime.Day == bookingTime.Day);
+            }
+
+            if (!string.IsNullOrEmpty(flightId))
+            {
+                query = query.Where(f => f.Id.ToString().Contains(flightId));
+            }
+            if (!string.IsNullOrEmpty(ticketId))
+            {
+                query = query.Where(f => f.Id.ToString().Contains(ticketId));
+            }
+
+            var ticketList = await query.Select(f => f).ToListAsync();
+
+            return View(ticketList);
         }
 
         // GET: Bookings/Details/5
@@ -46,106 +81,6 @@ namespace BookingTourWebApp_MVC.Controllers
             return View(booking);
         }
 
-        // GET: Bookings/Create
-        public IActionResult Create()
-        {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["FlightId"] = new SelectList(_context.Flights, "Id", "Id");
-            return View();
-        }
-
-        // POST: Bookings/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,AppUserId,FlightId,BusinessTickets,EconomyTickets,TotalPrice,BookingTime")] Booking booking)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(booking);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", booking.AppUserId);
-            ViewData["FlightId"] = new SelectList(_context.Flights, "Id", "Id", booking.FlightId);
-            return View(booking);
-        }
-
-        // GET: Bookings/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Bookings == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings.FindAsync(id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", booking.AppUserId);
-            ViewData["FlightId"] = new SelectList(_context.Flights, "Id", "Id", booking.FlightId);
-            return View(booking);
-        }
-
-        // POST: Bookings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,FlightId,BusinessTickets,EconomyTickets,TotalPrice,BookingTime")] Booking booking)
-        {
-            if (id != booking.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(booking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookingExists(booking.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", booking.AppUserId);
-            ViewData["FlightId"] = new SelectList(_context.Flights, "Id", "Id", booking.FlightId);
-            return View(booking);
-        }
-
-        // GET: Bookings/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Bookings == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings
-                .Include(b => b.AppUser)
-                .Include(b => b.Flight)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
 
         // POST: Bookings/Delete/5
         [HttpPost, ActionName("Delete")]
