e9bf47b792f850628be8a3bd671c7633aa47dd03
khoind2117 <khoind2117@gmail.com>
2023-12-03 13:30:35 +0700

    Sắp xếp folder lần 1

--- Program.cs
+++ Program.cs
@@ -67,6 +67,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TicketManagement}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
